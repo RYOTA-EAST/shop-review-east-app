5c2071eab7ab05202ea61678cd75211811048704
Ryota Higashi <ryo19870130@outlook.jp>
2022-02-15 00:17:57 +0900

    Create CreatReviewScreen

--- src/screens/ShopScreen.tsx
+++ src/screens/ShopScreen.tsx
@@ -2,6 +2,7 @@ import React, {useEffect} from "react";
 import { StyleSheet, SafeAreaView } from "react-native";
 /* components */
 import { ShopDetail } from "../components/ShopDetail";
+import { FloatingActionButton } from "../components/FlotingActionButton";
 /* types */
 import { RouteProp } from "@react-navigation/native";
 import { RootStackParamList } from "../types/navigation";
@@ -22,6 +23,10 @@ export const ShopScreen: React.FC<Props> = ({ navigation, route }: Props) => {
   return (
     <SafeAreaView style={styles.container}>
       <ShopDetail shop={shop} />
+      <FloatingActionButton 
+        iconName="plus"
+        onPress={() => navigation.navigate("CreateReview", { shop })}
+      />
     </SafeAreaView>
   );
 };
