f2731366deb4ab985ac844dfb3a8eafebd730add
Ryota Higashi <ryo19870130@outlook.jp>
2022-02-16 03:41:58 +0900

    Create AuthScreen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 /* navigator */
 import { MainTabNavigator } from "./MainTabNavigator";
+/* screens */
+import { AuthScreen } from "../screens/AuthScreen";
 
 export const AppNavigator = () => {
+  const user = {id: "123"};
   return (
     <NavigationContainer>
-      <MainTabNavigator />
+      {!user ? <AuthScreen /> : <MainTabNavigator />}
     </NavigationContainer>
   );
 };
